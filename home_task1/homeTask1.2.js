3b63334748b54b2aaf51d405ab30fcd2ceac50cf
Shailendra Singh <Shailendra_Singh@epam.com>
2021-03-21 08:56:27 +0530

    complete task 3

--- home_task1/homeTask1.2.js
+++ home_task1/homeTask1.2.js
@@ -1,17 +1,13 @@
 const csv = require('csvtojson');
 const path = require('path');
-const Promise = require('promise');
 
 const writeStream = require('fs').createWriteStream(path.resolve(__dirname, 'output.txt'));
 
 csv()
     .fromStream(require('fs').createReadStream(path.resolve(__dirname, 'nodejs-hw1-ex1.csv')))
     .subscribe((json) => {
-        return new Promise((resolve, _reject) => {
-            delete json['Amount']
-            writeStream.write(JSON.stringify(json) + '\r\n');
-            resolve();
-        })
+        delete json['Amount']
+        writeStream.write(JSON.stringify(json) + '\r\n');
     }, (error) => {
         console.log(error);
     }, () => {
