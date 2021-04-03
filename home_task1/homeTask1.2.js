ef820945f5e033ec16cde861e6c5f975ab22c34d
Shailendra Pratap Singh <shailendra.cs09@gmail.com>
2021-04-03 09:01:39 +0530

    added error handling

--- home_task1/homeTask1.2.js
+++ home_task1/homeTask1.2.js
@@ -1,21 +1,27 @@
 const csv = require('csvtojson');
 const path = require('path');
+const fs = require('fs');
 
-try {
-    const writeStream = require('fs').createWriteStream(path.resolve(__dirname, 'output.txt'));
-    csv({ ignoreColumns: /(amount)/ }).preFileLine((fileLine, idx) => {
-        if (idx === 0) { return fileLine.toLowerCase(); }
-        return fileLine;
-    })
-        .fromStream(require('fs').createReadStream(path.resolve(__dirname, 'nodejs-hw1-ex1.csv')))
-        .subscribe((json) => {
-            writeStream.write(JSON.stringify(json) + '\r\n');
-        }, (error) => {
-            console.log(error);
-        }, () => {
-            writeStream.close();
-        });
-}
-catch (error) {
-    console.log('error occured while performing action');
-}
+
+const writeStream = fs.createWriteStream(path.resolve(__dirname, 'output.txt'));
+const readStream = fs.createReadStream(path.resolve(__dirname, 'nodejs-hw1-ex1.csv'));
+
+writeStream.on('error', () => {
+    console.log(`error in writing the file ${path.resolve(__dirname, 'output3.txt')}`);
+});
+readStream.on('error', () => {
+    console.log(`error in reading the file ${path.resolve(__dirname, 'nodejs-hw1-ex1.csv')}`);
+});
+
+csv({ ignoreColumns: /(amount)/ }).preFileLine((fileLine, idx) => {
+    if (idx === 0) { return fileLine.toLowerCase(); }
+    return fileLine;
+})
+    .fromStream(readStream)
+    .subscribe((json) => {
+        writeStream.write(JSON.stringify(json) + '\r\n');
+    }, (error) => {
+        console.log(error);
+    }, () => {
+        writeStream.close();
+    });
--- home_task1/homeTask1.3.js
+++ home_task1/homeTask1.3.js
@@ -1,24 +1,26 @@
 import csv from 'csvtojson';
-import  path from 'path';
+import path from 'path';
 import fs from 'fs';
 
+const writeStream = fs.createWriteStream(path.resolve(__dirname, 'output3.txt'));
+const readStream = fs.createReadStream(path.resolve(__dirname, 'nodejs-hw1-ex1.csv'));
 
+writeStream.on('error', () => {
+    console.log(`error in writing the file ${path.resolve(__dirname, 'output3.txt')}`);
+});
+readStream.on('error', () => {
+    console.log(`error in reading the file ${path.resolve(__dirname, 'nodejs-hw1-ex1.csv')}`);
+});
 
-try {
-    const writeStream = fs.createWriteStream(path.resolve(__dirname, 'output3.txt'));
-    csv({ ignoreColumns: /(amount)/ }).preFileLine((fileLine, idx) => {
-        if (idx === 0) { return fileLine.toLowerCase(); }
-        return fileLine;
-    })
-        .fromStream(require('fs').createReadStream(path.resolve(__dirname, 'nodejs-hw1-ex1.csv')))
-        .subscribe((json) => {
-            writeStream.write(JSON.stringify(json) + '\r\n');
-        }, (error) => {
-            console.log(error);
-        }, () => {
-            writeStream.close();
-        });
-}
-catch (error) {
-    console.log('error occured while performing action');
-}
+csv({ ignoreColumns: /(amount)/ }).preFileLine((fileLine, idx) => {
+    if (idx === 0) { return fileLine.toLowerCase(); }
+    return fileLine;
+})
+    .fromStream(readStream)
+    .subscribe((json) => {
+        writeStream.write(JSON.stringify(json) + '\r\n');
+    }, (error) => {
+        console.log(error);
+    }, () => {
+        writeStream.close();
+    });
